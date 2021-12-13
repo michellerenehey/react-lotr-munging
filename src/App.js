bbea6d956ad83d7d477b4721e22fcb48b5b08fbb
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-13 12:58:38 -0800

    added some notes for getFilms() function

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   const getFilms = async () => {
     // Add your code here!
-    // 1. Get data using fetch from https://the-one-api.dev/v2/movie/ (don't forget to set your header!)
+    // x 1. Get data using fetch from https://the-one-api.dev/v2/movie/ (don't forget to set your header!)
     // 2. Transform the response so that films contains nested arrays of:
     //   - the film's title
     //   - the film's title "slugified" i.e. in all lower case, with words separated with dashes,
@@ -27,7 +27,42 @@ function App() {
 
     // 3. Set the resulting transformation as state using setFilms
     // 4. You'll know it works if the films show up on the page
+
+    const response = await fetch('https://the-one-api.dev/v2/movie/', {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+      },
+    });
+    const data = await response.json();
+    // do a data.map to run through the data
+
+    // here is what the data look slike
+    //   {
+    //     "_id": "5cd95395de30eff6ebccde56",
+    //     "name": "The Lord of the Rings Series",
+    //     "runtimeInMinutes": 558,
+    //     "budgetInMillions": 281,
+    //     "boxOfficeRevenueInMillions": 2917,
+    //     "academyAwardNominations": 30,
+    //     "academyAwardWins": 17,
+    //     "rottenTomatoesScore": 94
+    // }
+
+    // transform the above data to look like this:
+    // [["The Lord of the Rings Series", "the-lord-of-the-rings-series", 2917, 30 ], ["The Hobbit Series", "the-hobit-series", 2932, 7]...]
+
+    // setFilm in this function, too
+
+    // here is what the pokedex fetch looked like:
+    // const fetchData = async () => {
+    //   const data = await getPokemon(query, order, selectedType, currentPage);
+    //   // console.log(data);
+    //   setPokemonList(data.results);
+    //   setLoading(false);
+    // };
+
     return [];
+    // can leave line 50 as it is (or delete it?)
   };
 
   const getCharacters = async () => {
@@ -58,10 +93,10 @@ function App() {
           </NavLink>
         </header>
         <Switch>
-          <Route path="/films">
+          <Route exact path="/films">
             <h1>hello 1</h1>
           </Route>
-          <Route path="/characters">
+          <Route exact path="/characters">
             <h1>hello 2</h1>
           </Route>
         </Switch>
