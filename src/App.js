ca41d001b2de0840995fe0c13615672f770ac021
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-13 16:34:51 -0800

    cleaned up console.logs

--- src/App.js
+++ src/App.js
@@ -40,41 +40,14 @@ function App() {
       },
     });
     const data = await response.json();
-    console.log('characters full data', data);
-
     const characterData = data.map((item) => ({
       name: item.name,
       birth: item.birth,
       death: item.death,
       dates: item.birth === item.death ? 'Unknown' : `${item.birth} - ${item.death}`,
     }));
-    console.log('characters partial', characterData);
-
     setCharacters(characterData);
   };
-
-  // dates one use a ternary (condiitonally set the property)
-  // propterty: something ? someValue : someOtherValue
-
-  // birth: "Before the Shaping of Arda"
-  // created_at: "2021-12-13T22:47:37+00:00"
-  // death: "01/25/3019"
-  // id: 1
-  // name: "Gandalf"
-  // race: "Maiar"
-
-  // Add your code here!
-  // 1. Get data using fetch from https://the-one-api.dev/v2/character/
-  // 2. Update the response data with the key `dates` which is a combination of
-  //    the `birth` key and the `death key` separated with a dash. If neither date
-  //    is provided, it should hold the string 'Unknown'
-  //    [
-  //       {name: 'Adanel', birth: "", death: "", dates: "Unknown", ...},
-  //       {name: 'Adrahil I', birth: "Before , TA 1944", death: "Late , Third Age", dates: "Before , TA 1944 - Late , Third Age", ...},
-  //       {name: 'Adrahil II', birth: "TA 2917", death: "TA 3010, dates: "TA 2917 - TA 3010", ...},
-  //    ]
-  // 3. Set the resulting transformation as state using setCharacters
-  // 4. You'll know it works if the characters show up on the page
 
   return (
     <div className="App">
